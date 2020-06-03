83f1a4d326c250a758b1a314d7f218d50e39092a
chong <mac968@sina.com>
2020-06-03 15:41:34 +0800

    搭建01

---
+++ yt_outh/src/main/java/com/yt/YtOuthApplication.java
@@ -0,0 +1,13 @@
+package com.yt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YtOuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YtOuthApplication.class, args);
+    }
+
+}
---
+++ yt_registered/src/main/java/com/yt/YtRegisteredApplication.java
@@ -0,0 +1,13 @@
+package com.yt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YtRegisteredApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YtRegisteredApplication.class, args);
+    }
+
+}
